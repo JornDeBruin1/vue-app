4f303df0264aa1e6d80a41a7d18fb97c565ca871
Jorn de Bruin <jorn.bruin@student.graafschapcollege.nl>
2023-11-07 13:21:11 +0100

    making memory list and item easy to change

---
+++ src/components/memories/MemoryListItem.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <ion-item :router-link="`/memories/${memory.id}`">
+            <IonThumbnail slot="start">
+                <IonImg :src="memory.image" :alt="memory.title"></IonImg>
+            </IonThumbnail>
+            <IonLabel>
+                {{ memory.title }}
+            </IonLabel>      
+        </ion-item>
+    </div>
+</template>
+<script>
+import{IonItem, IonThumbnail, IonImg, IonLabel } from '@ionic/vue'
+
+export default {
+    props:['memory'],
+    components:{
+        IonItem,
+        IonThumbnail, 
+        IonImg, 
+        IonLabel
+    }   
+}
+</script>
